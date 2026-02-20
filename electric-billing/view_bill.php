55052b9c90145065b34312d978fa1ba623b914a8
AbeaAquino <abeaaquino29@gmail.com>
2026-02-20 21:43:27 +0800

    Update view_bill.php

--- electric-billing/view_bill.php
+++ electric-billing/view_bill.php
@@ -39,17 +39,20 @@ $bills = $bills_query->get_result();
     </div>
 
     <div class="nav-links">
-        <a href="home.php" class="<?php echo ($current_page == 'home') ? 'active' : ''; ?>">Home</a>
+        <a href="home.php">Home</a>
         <span class="divider">|</span>
         <a href="view_bill.php" class="active">View Bill</a>
         <span class="divider">|</span>
-        <a href="pay_bill.php" class="<?php echo ($current_page == 'pay_bill') ? 'active' : ''; ?>">Pay Bill</a>
+        <a href="pay_bill.php">Pay Bill</a>
         <span class="divider">|</span>
+
         <!-- ACCOUNT ICON -->
         <?php if (isset($_SESSION['user_id'])): ?>
-            <a href="account.php">
+            <a href="account.php" style="line-height:0;">
                 <div class="account-icon">
-                    <img src="assets/account-icon.png" alt="Account">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="#173a7a">
+                        <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z"/>
+                    </svg>
                 </div>
             </a>
         <?php endif; ?>
