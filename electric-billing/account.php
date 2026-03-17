00ab4dbcaec1a18cf4822f18ef62d57ebfae9aef
AbeaAquino <abeaaquino29@gmail.com>
2026-03-17 17:49:52 +0800

    Update account.php

--- electric-billing/account.php
+++ electric-billing/account.php
@@ -61,6 +61,13 @@ $due = $due_query->get_result()->fetch_assoc();
             Payment History
         </a>
 
+        <a href="edit_profile.php" class="dropdown-item">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#555">
+                <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z"/>
+            </svg>
+            Edit Profile
+        </a>
+
         <!-- Log Out -->
         <a href="logout.php" class="dropdown-item logout">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#e53935">
