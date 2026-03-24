fcfc14173c49ad8a6e5de8b946324103275f852d
AbeaAquino <abeaaquino29@gmail.com>
2026-03-24 17:02:20 +0800

    Modify admin password and add new data entries
    Updated admin password to use hashed value and added new advisories and bills data.

--- electric-billing/angeles_electric_db.sql
+++ electric-billing/angeles_electric_db.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 17, 2026 at 10:41 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `angeles_electric_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admins`
---
 
 CREATE TABLE `admins` (
   `id` int(11) NOT NULL,
@@ -33,18 +8,8 @@ CREATE TABLE `admins` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `admins`
---
-
 INSERT INTO `admins` (`id`, `admin_id`, `password`) VALUES
-(1, '1001', 'admin_123');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `advisories`
---
+(1, '1001', '$2y$10$DIdythi.4I6HZlYLANOja.yr8RqBHFR6S5aGjY6l.4g1Od/hxOtyG');
 
 CREATE TABLE `advisories` (
   `id` int(11) NOT NULL,
@@ -54,41 +19,19 @@ CREATE TABLE `advisories` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `advisories`
---
-
 INSERT INTO `advisories` (`id`, `title`, `category_id`, `description`, `created_at`) VALUES
-(28, 'PLANNED POWER INTERRUPTION', 3, 'Date: July 15, 2026\nTime: 9:30 PM\nAffected Area/s: Angeles\n\nPlease be advice of the power outage in your area\n\nAdditional Notes: We apologize for the yes', '2026-03-17 08:31:51'),
-(29, 'Monthly Maintenance ', 2, 'Date: March 19, 2026\nTime: 8:40 PM - 10:40 PM\nAffected Area/s: Angeles\n\nTest GO TO SM FOR FREE ELECTRIC\n\nAdditional Notes: Pls be careful', '2026-03-17 08:41:03'),
-(30, 'Monthly Maintenance ', 2, 'Date: 2026-03-19\nTime: \nAffected Area/s: Angeles\n\nMISS MY BEBEE\n\nAdditional Notes: ', '2026-03-17 08:42:07'),
-(31, 'Test', 2, 'Date: March 21, 2026\nTime: 6:00 AM - 8:00 PM\nAffected Area/s: Angeles\n\nROAR\n\nAdditional Notes: ', '2026-03-17 08:44:23');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `advisory_categories`
---
+(33, 'Billing Reminder and Payment Notice', 1, 'Date: March 31, 2026\nTime: 11:00 AM - 1:00 AM\nAffected Area/s: \n\nThis is to inform all concerned users that billing statements for the current period have been issued. Please ensure that payments are settled on or before the due date to avoid service interruptions or additional charges.\n\nAdditional Notes: Kindly verify your billing details and contact support immediately for any discrepancies. Multiple payment methods are available for your convenience.', '2026-03-24 08:16:36'),
+(34, 'Power Interruption Advisory', 3, 'Date: March 25, 2026\nTime: 2:30 PM - 1:00 AM\nAffected Area/s: Sto. Rosario, Angeles City\n\nThis is to notify all affected users in Sto. Rosario, Angeles City of a scheduled power interruption. This may result in temporary service disruptions.\n\nAdditional Notes: Please take necessary precautions and plan accordingly. Services will be restored once power supply is normalized. We appreciate your understanding.', '2026-03-24 08:19:06');
 
 CREATE TABLE `advisory_categories` (
   `id` int(11) NOT NULL,
   `category_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `advisory_categories`
---
-
 INSERT INTO `advisory_categories` (`id`, `category_name`) VALUES
 (1, 'Billing Advisories'),
 (2, 'Maintenance Schedule'),
 (3, 'Power Interruption Notice');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bills`
---
 
 CREATE TABLE `bills` (
   `id` int(11) NOT NULL,
@@ -100,21 +43,9 @@ CREATE TABLE `bills` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `bills`
---
-
 INSERT INTO `bills` (`id`, `user_id`, `billing_month`, `amount`, `due_date`, `status`, `created_at`) VALUES
-(8, 2, 'March 2026', 45000.25, '2026-04-30', 'Paid', '2026-03-17 05:11:42'),
-(9, 2, 'Feb 2026', 3000.00, '2026-02-04', 'Paid', '2026-03-17 05:15:01'),
-(12, 1, 'March 2026', 300000.00, '2026-04-23', 'Unpaid', '2026-03-17 06:34:01'),
-(13, 2, 'Jan 2026', 44000.00, '2026-03-30', 'Unpaid', '2026-03-17 08:47:59');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `payments`
---
+(14, 5, 'March 2026', 4000.00, '2026-03-31', 'Paid', '2026-03-24 07:55:58'),
+(15, 5, 'April 2026', 5000.00, '2026-04-30', 'Unpaid', '2026-03-24 07:56:18');
 
 CREATE TABLE `payments` (
   `id` int(11) NOT NULL,
@@ -126,39 +57,18 @@ CREATE TABLE `payments` (
   `payment_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `payments`
---
-
 INSERT INTO `payments` (`id`, `user_id`, `bill_id`, `payment_method_id`, `amount`, `reference_number`, `payment_date`) VALUES
-(1, 2, 8, 1, 4500.00, 'REF79566', '2026-03-17 05:25:54'),
-(2, 2, 9, 1, 2000.00, 'REF46904', '2026-03-17 05:26:05');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `payment_methods`
---
+(5, 5, 14, 3, 4000.00, 'REF73059', '2026-03-24 07:58:06');
 
 CREATE TABLE `payment_methods` (
   `id` int(11) NOT NULL,
   `method_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `payment_methods`
---
-
 INSERT INTO `payment_methods` (`id`, `method_name`) VALUES
 (3, 'Bank Transfer'),
 (2, 'Cash'),
 (1, 'GCash');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -172,140 +82,71 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `account_number`, `name`, `email`, `password`, `dob`, `address`, `contact_no`, `created_at`) VALUES
-(1, '100001', 'Test User', 'test@email.com', '123456', '2000-01-01', 'Angeles City', '09123456789', '2026-03-15 11:43:10'),
-(2, '295696026', 'Abea Aquino', 'abeaaquino29@gmail.com', '54321', '3342-02-01', '316 st.matthew purok 7', '20629675', '2026-03-15 11:48:15');
+(3, '466712199', 'Ryence Cortez', 'abea@gmail.com', '$2y$10$KDVkn4WYzU.rp6328ITLc.21jHyBXxIKa/97ZsUSTxlKo9gz7eFWG', '2025-02-04', '04 st.matthew purok 7', '12345678910', '2026-03-24 06:14:52'),
+(5, '538882179', 'Abea Aquino', 'abeaaquino29@gmail.com', '$2y$10$k.rrbAhyTqDvaAEn6OFPA.vmjHFZdOY4KFbH4v7o4gkcERgz4/Bwe', '2006-07-29', 'Angeles City', '09262287984', '2026-03-24 06:46:15'),
+(6, '923435908', 'Test User', 'test@email.com', '$2y$10$UvwugZtwb.dQZ2MmioXs1ubRIYgiyJoGniqplX6kHiCHcA1oj5GkS', '2024-07-29', 'Sto. Rosario, Angeles City', '09212387984', '2026-03-24 08:21:16');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admins`
---
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `admin_id` (`admin_id`);
 
---
--- Indexes for table `advisories`
---
 ALTER TABLE `advisories`
   ADD PRIMARY KEY (`id`),
   ADD KEY `category_id` (`category_id`);
 
---
--- Indexes for table `advisory_categories`
---
 ALTER TABLE `advisory_categories`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `bills`
---
 ALTER TABLE `bills`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `unique_user_month` (`user_id`,`billing_month`);
 
---
--- Indexes for table `payments`
---
 ALTER TABLE `payments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `bill_id` (`bill_id`),
   ADD KEY `payment_method_id` (`payment_method_id`);
 
---
--- Indexes for table `payment_methods`
---
 ALTER TABLE `payment_methods`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `method_name` (`method_name`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `account_number` (`account_number`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admins`
---
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `advisories`
---
 ALTER TABLE `advisories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
---
--- AUTO_INCREMENT for table `advisory_categories`
---
 ALTER TABLE `advisory_categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table `bills`
---
 ALTER TABLE `bills`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
---
--- AUTO_INCREMENT for table `payments`
---
 ALTER TABLE `payments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table `payment_methods`
---
 ALTER TABLE `payment_methods`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `advisories`
---
 ALTER TABLE `advisories`
   ADD CONSTRAINT `advisories_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `advisory_categories` (`id`);
 
---
--- Constraints for table `bills`
---
 ALTER TABLE `bills`
   ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
---
--- Constraints for table `payments`
---
 ALTER TABLE `payments`
   ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`id`),
   ADD CONSTRAINT `payments_ibfk_3` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_methods` (`id`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
