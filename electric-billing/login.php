2436b43347f95328aada9dc4dea8b067bb212b84
AbeaAquino <abeaaquino29@gmail.com>
2026-03-24 16:48:03 +0800

    Improve password handling in login process
    Replaced password comparison with password_verify for security.

--- electric-billing/login.php
+++ electric-billing/login.php
@@ -13,7 +13,7 @@ if(isset($_POST['login'])){
     if($result->num_rows > 0){
         $user = $result->fetch_assoc();
 
-        if($password === $user['password']){
+        if(password_verify($password, $user['password'])){
             $_SESSION['user_id'] = $user['id'];
             header("Location: home.php");
             exit();
